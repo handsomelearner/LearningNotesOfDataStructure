9b5bd9322fa68b152ea4452c4bd3552f2a0d3d26
handsomelearner <56440889+handsomelearner@users.noreply.github.com>
2019-10-14 22:10:54 +0800

    Create 创建二叉树不懂哪里错了.cpp

---
+++ 创建二叉树不懂哪里错了.cpp
@@ -0,0 +1,35 @@
+BinTree PlantABinTree(){
+    BinTree BT,T;
+    ElementType Data;
+    cin>>Data;
+    vector<BinTree>Q;
+    cin>>Data;
+    if(Data!=0){
+        BT=(BinTree)malloc(sizeof(struct BNode));
+        BT->Data=Data;
+        BT->Left=BT->Right=NULL;
+        Q.push_back(BT);
+    }
+    else return NULL;
+    while(Q.size()>0){
+        T=Q[0];
+        Q.erase(Q.begin());
+        cin>>Data;
+        if(Data==0) T->Left=NULL;
+        else{
+            T->Left=(BinTree)malloc(sizeof(struct BNode));
+            (T->Left)->Data=Data;
+            (T->Left)->Left=(T->Left)->Right=NULL;
+            Q.push_back(T->Left);
+        }
+        cin>>Data;
+        if(Data==0) T->Right=NULL;
+        else{
+            T->Right=(BinTree)malloc(sizeof(struct BNode));
+            (T->Right)->Data=Data;
+            (T->Right)->Left=(T->Right)->Right=NULL;
+            Q.push_back(T->Left);
+        }
+    }
+    return BT;
+}
