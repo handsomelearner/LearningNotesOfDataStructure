30f761f0f0b54503efb4ec48b94e068997587ab0
handsomelearner <56440889+handsomelearner@users.noreply.github.com>
2019-10-14 22:53:56 +0800

    Update 遍历二叉树.cpp

--- 遍历二叉树.cpp
+++ 遍历二叉树.cpp
@@ -2,6 +2,11 @@
 #include<string>
 #include<algorithm>
 #include<vector>
+/////////////////////////
+#include <iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
 #define MAXSIZE 10010
 /////////////////////////
 typedef char ElementType;
@@ -12,69 +17,58 @@ struct BNode{
     BinTree Left,Right;
 };
 /////////////////////////////////
-typedef struct QNode * Queue;
-typedef int Position;
-struct QNode{
-    BinTree * Data;
-    Position Front,Rear;
-    int MaxSize;
-};
-/////////////////////////
 BinTree PlantABinTree();
 void First(BinTree BT);
 void Mid(BinTree BT);
 void Last(BinTree BT);
 /////////////////////
-Queue CreatAnEmptyQueue();
-bool QueueIsEmpty(Queue Q);
-BinTree DeleteQ(Queue Q);
-void AddQ(Queue Q,BinTree BT);
-/////////////////////
 using namespace std;
 ////////////////////
 int main(int argc, const char * argv[]) {
-    
-    cout<<"enter: ";
     BinTree BT=PlantABinTree();
     First(BT);
     cout<<"\n";
     Mid(BT);
     cout<<"\n";
     Last(BT);
+     cout<<"\n";
     return 0;
 }
 ///////////////////
 BinTree PlantABinTree(){
     BinTree BT,T;
     ElementType Data;
-    Queue Q=CreatAnEmptyQueue();
+    vector<BinTree>Q;
     cin>>Data;
-    if(Data){
+    if(Data!='0'){
         BT=(BinTree)malloc(sizeof(struct BNode));
         BT->Data=Data;
         BT->Left=BT->Right=NULL;
-        AddQ(Q,BT);
+        Q.push_back(BT);
     }
     else return NULL;
-        while(!QueueIsEmpty(Q)){
-            T=DeleteQ(Q);
-            cin>>Data;
-            if(Data==0) T->Left=NULL;
-            else{
+    
+    while(Q.size()!=0){
+        T=Q[0];
+        Q.erase(Q.begin());
+        cin>>Data;
+        if(Data=='0') T->Left=NULL;
+        else{
             T->Left=(BinTree)malloc(sizeof(struct BNode));
             (T->Left)->Data=Data;
             (T->Left)->Left=(T->Left)->Right=NULL;
-            AddQ(Q,T->Left);
-            }
-            cin>>Data;
-            if(Data==0) T->Right=NULL;
-            else{
-                T->Right=(BinTree)malloc(sizeof(struct BNode));
-                (T->Right)->Data=Data;
-                (T->Right)->Left=(T->Right)->Right=NULL;
-                AddQ(Q, T->Right);
-            }
+            Q.push_back(T->Left);
         }
+        cin>>Data;
+        if(Data=='0') T->Right=NULL;
+        else{
+            T->Right=(BinTree)malloc(sizeof(struct BNode));
+            (T->Right)->Data=Data;
+            (T->Right)->Left=(T->Right)->Right=NULL;
+            Q.push_back(T->Right);
+        }
+    }
+    
     return BT;
 }
 void First(BinTree BT){
@@ -86,32 +80,16 @@ void First(BinTree BT){
     }
 }
 void Mid(BinTree BT){
-    First(BT->Left);
+    if(BT){
+    Mid(BT->Left);
      cout<<BT->Data;
-    First(BT->Right);
+    Mid(BT->Right);
+    }
 }
 void Last(BinTree BT){
     if(BT){
-        First(BT->Left);
-        First(BT->Right);
+        Last(BT->Left);
+        Last(BT->Right);
         cout<<BT->Data;
     }
-}
-////////////////////////
-Queue CreatAnEmptyQueue(){
-    Queue Q=(Queue)malloc(sizeof(struct QNode));
-    Q->Data=(BinTree*)malloc(MAXSIZE*sizeof(BinTree));
-    Q->Front=Q->Rear=0;
-    Q->MaxSize=MAXSIZE;
-    return Q;
-}
-bool QueueIsEmpty(Queue Q){
-    if(Q->Front==Q->Rear)return true;
-    return false;
-}
-BinTree DeleteQ(Queue Q){
-    return Q->Data[(Q->Front)++];
-}
-void AddQ(Queue Q,BinTree BT){
-    Q->Data[++(Q->Rear)]=BT;
 }
